221147973eb60d4bf4537c9109c0b310446c370b
JNethaduna <147524613+JNethaduna@users.noreply.github.com>
2024-03-08 11:46:03 +0530

    Updates

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -5,11 +5,16 @@
 		'pk.eyJ1IjoiYXNobnRoIiwiYSI6ImNsc282YW9hMzBieXkybW5zOXF3YWgyMWMifQ.l54caXaNFKAtBj1n_CFm8w';
 	const center: [number, number] = [80.04144677117178, 6.821314369940506];
 	const zoom = 18;
-	const coordinates = [80.04144677117178, 6.821314369940506];
+	const markers: GeoJSON[] = [
+		{ type: 'Point', coordinates: [80.04144677117178, 6.821314369940506] },
+		{ type: 'Point', coordinates: [79.09144677117157, 6.871314369940576] }
+	];
 </script>
 
 <body class="w-svh h-svh">
 	<Map {accessToken} {center} {zoom}>
-		<Marker lng={coordinates[0]} lat={coordinates[1]} />
+		{#each markers as marker}
+			<Marker lng={marker.coordinates[0]} lat={marker.coordinates[1]} />
+		{/each}
 	</Map>
 </body>
